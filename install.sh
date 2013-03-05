726653816660f0c071342a63ff6be4dd50efa1ff
Supasorn Suwajanakorn <supasorn@Supasorns-MBP.local>
2013-03-04 19:44:19 -0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+    echo "Installing Vundle"
+    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
+vim -c "BundleInstall"
