079c6d3b6e961e7c0b5827a4203203f50c6efbce
supasorn <supasorn@gmail.com>
2018-12-15 21:43:02 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 
-if [ ! -e $HOME/.vim/bundle/vundle ]; then
-    echo "Installing Vundle"
-    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-fi
-
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 
-vim +BundleInstall! +BundleClean +qall
+vim +PlugInstall +qall
