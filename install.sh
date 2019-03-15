246a5f0c12af6190a6fa8cff0fc23a114e4ccc41
supasorn <supasorn@gmail.com>
2019-03-15 18:27:10 +0700

    auto commit

--- install.sh
+++ install.sh
@@ -4,6 +4,14 @@ ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 ln -sf ~/.vim/tmux.conf ~/.tmux.conf
 ln -sf ~/.vim/zshrc ~/.zshrc
+
+mkdir -p ~/.config/nvim
 cp ~/.vim/init.vim ~/.config/nvim/init.vim
 
+cd ~/.vim/plugged/FuzzyFinder
+curl -Lo fix-top-left-focus-bug.patch https://github.com/vim-scripts/FuzzyFinder/pull/2.diff
+patch -p1 <fix-top-left-focus-bug.patch
+
 vim +PlugInstall +qall
+
+mv ~/.vim/key-bindings.zsh ~/.fzf/shell/ 
