7995f04657ca4077dc45137e450cf44a36e08588
supasorn <supasorn@gmail.com>
2019-03-15 16:49:58 +0700

    add init.vim

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 ln -sf ~/.vim/tmux.conf ~/.tmux.conf
 ln -sf ~/.vim/zshrc ~/.zshrc
+cp ~/.vim/init.vim ~/.config/nvim/init.vim
 
-#vim +PlugInstall +qall
+vim +PlugInstall +qall
