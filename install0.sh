0eb021dc91a1c9d6df53cb142a2ce78494c6b675
supasorn <supasorn@gmail.com>
2019-09-18 23:58:35 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -2,5 +2,5 @@
 sudo apt -y install neovim vim zsh tmux curl
 chsh -s /usr/bin/zsh
 git clone https://github.com/supasorn/vim.git ~/.vim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
