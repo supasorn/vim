428b51db64ddf9667aac08faa0d9f7fbfa37d50e
supasorn <supasorn@gmail.com>
2019-09-19 00:00:28 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -4,3 +4,4 @@ chsh -s /usr/bin/zsh
 git clone https://github.com/supasorn/vim.git ~/.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 sh ~/.vim/install.sh
+zsh
