4e158997886473e1eccb5e54ec016afcaab919fa
supasorn <supasorn@gmail.com>
2019-10-24 11:32:10 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
 sudo apt -y install neovim vim zsh tmux curl ripgrep
