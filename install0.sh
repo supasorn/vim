afad752b48ab77bea036d2a711d4d988b1e57519
supasorn <supasorn@gmail.com>
2021-09-03 11:58:45 +0700

    Update install0.sh

--- install0.sh
+++ install0.sh
@@ -2,7 +2,7 @@
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/supasorn/vim/master/install0.sh)"
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
-sudo apt -y install neovim vim zsh tmux curl ripgrep git
+sudo apt -y install neovim vim zsh tmux curl ripgrep git exuberant-ctags
 chsh -s /usr/bin/zsh
 git clone https://github.com/supasorn/vim.git ~/.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
